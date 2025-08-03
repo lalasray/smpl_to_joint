9144020e173f4996c6161b7eeacff3c59dbcb57b
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2025-08-03 16:12:46 +0200

    Update imusim_SMPL.py

--- imusim_SMPL.py
+++ imusim_SMPL.py
@@ -117,4 +117,4 @@ def process_directory(root_dir, sf):
 
 
 # ===== Example usage =====
-process_directory("/home/lala/Documents/Data/VQIMU/UTD_MHAD", sf=15)
+process_directory("/home/lala/Documents/Data/VQIMU/MMFIT", sf=15)
